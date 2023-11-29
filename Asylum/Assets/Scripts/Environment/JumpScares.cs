1704fa8b79d4598abefca98adb3e1f887658a3c7
KacperZmu <kacper.zmudzinski@students.plymouth.ac.uk>
2023-11-29 12:34:59 +0000

    gameplay changes
    added barricades, jump scare - wall texture for temp, removed some pills

---
+++ Asylum/Assets/Scripts/Environment/JumpScares.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScares : MonoBehaviour
+{
+  
+    // Reference to the object with the animation
+    public GameObject animatedObject;
+    public AudioSource audioData1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the entering object has the "Player" tag
+        if (other.CompareTag("Player"))
+        {
+            // Play the animation on the animatedObject
+            Animator animator = animatedObject.GetComponent<Animator>();
+            if (animator != null)
+            {
+                // Trigger the animation by setting a parameter
+                audioData1.Play(0);
+                animator.SetTrigger("Scare");
+            }
+            else
+            {
+                Debug.LogError("Animator component not found on the animatedObject.");
+            }
+        }
+    }
+}
+
+
