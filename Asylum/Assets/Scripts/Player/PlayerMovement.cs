8210f513361d496847c1cca1ade8d3e15a50d9bb
KacperZmu <kacper.zmudzinski@students.plymouth.ac.uk>
2023-12-06 21:46:16 +0000

    Improved crouch
    Player can crouch under objects with a tag, while under these the button doesnt need to be held down. once player comes out from under object they are auto returned to starting height

--- Asylum/Assets/Scripts/Player/PlayerMovement.cs
+++ Asylum/Assets/Scripts/Player/PlayerMovement.cs
@@ -62,6 +62,10 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
     public CapsuleCollider playerCollider;
 
+    bool canStandUp = true;
+    private bool isDelayedStandUp = false;
+    private float standUpDelayTimer = 0f;
+    private readonly float standUpDelayDuration = 0.2f;
 
     private void Start()
     {
@@ -77,14 +81,63 @@ public class PlayerMovement : MonoBehaviour
         beginYScale = transform.localScale.y;
     }
 
+    
+
     private void Update()
     {
-        // ground check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
-        //stairClimb();
         MyInput();
         SpeedControl();
 
+        // Perform a raycast to check for obstacles above the player's head
+        RaycastHit hit;
+        bool hitObstacle = Physics.Raycast(transform.position, Vector3.up, out hit, playerHeight * 0.5f + 0.2f);
+
+        // Visualize the ray for standing up
+        Debug.DrawRay(transform.position, Vector3.up * (playerHeight * 0.5f + 0.2f), hitObstacle ? Color.red : Color.green);
+
+        if (hitObstacle && hit.collider.CompareTag("Crouched"))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            canStandUp = false;
+            ResetStandUpDelay();
+        }
+        else
+        {
+            canStandUp = true;
+
+            // Use your original logic to stand up when the crouch key is released
+            if (canStandUp && Input.GetKeyUp(crouchKey))
+            {
+                TryStandUp();
+            }
+
+            // Auto-stand if there's no obstacle above and the crouch button is not pressed after the delay
+            if (!hitObstacle && !Input.GetKey(crouchKey))
+            {
+                if (isDelayedStandUp)
+                {
+                    standUpDelayTimer += Time.deltaTime;
+
+                    if (standUpDelayTimer >= standUpDelayDuration)
+                    {
+                        transform.localScale = new Vector3(transform.localScale.x, beginYScale, transform.localScale.z);
+                        ResetStandUpDelay();
+                    }
+                }
+                else
+                {
+                    // Start the delay timer
+                    isDelayedStandUp = true;
+                }
+            }
+        }
+
+        if (!grounded)
+        {
+            canStandUp = false; // Player cannot stand up in the air
+            ResetStandUpDelay(); // Reset the delay if the player is not grounded
+        }
 
         // handle drag
         if (grounded)
@@ -92,6 +145,42 @@ public class PlayerMovement : MonoBehaviour
         else
             rb.drag = 0;
     }
+
+    private void ResetStandUpDelay()
+    {
+        isDelayedStandUp = false;
+        standUpDelayTimer = 0f;
+    }
+
+
+
+    private void TryStandUp()
+    {
+        // Perform a raycast to check for obstacles above the player's head
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.up, out hit, playerHeight * 0.5f + 0.2f))
+        {
+            // Check if the object hit has a specific tag for crouching
+            if (hit.collider.CompareTag("Crouched"))
+            {
+                transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+                canStandUp = false;
+                Debug.Log("Hit a crouched object. Cannot stand up.");
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x, beginYScale, transform.localScale.z);
+                Debug.Log("Hit an object. Allowing stand up.");
+            }
+        }
+        else
+        {
+            // No obstacle above, allow standing up
+            transform.localScale = new Vector3(transform.localScale.x, beginYScale, transform.localScale.z);
+            Debug.Log("No obstacle above");
+        }
+    }
+
 
     private void FixedUpdate()
     {
